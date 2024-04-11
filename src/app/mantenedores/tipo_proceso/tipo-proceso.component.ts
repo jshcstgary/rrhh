3960b40b35bf167f0756b50d00aea21eda0e04e7
Tony <tveascervantes@hotmail.com>
2024-04-11 08:24:55 -0500

    tvc - 11042024 - refactor(frontend): refactor tipo-motivo bug tipo solicitudes

--- src/app/mantenedores/tipo_proceso/tipo-proceso.component.ts
+++ src/app/mantenedores/tipo_proceso/tipo-proceso.component.ts
@@ -1,34 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { IColumnsTable } from 'src/app/component/table/table.interface';
-import { TipoprocesoData } from './tipo-proceso.data';
-import { ITipoproceso, ITipoprocesoTable} from './tipo-proceso.interface';
-import Swal from 'sweetalert2';
-import { HttpErrorResponse } from '@angular/common/http';
-import { IInputsComponent } from 'src/app/component/input/input.interface';
-import { reportCodeEnum } from 'src/app/services/util/util.interface';
-import { TableService } from 'src/app/component/table/table.service';
-import { ValidationsService } from 'src/app/services/validations/validations.service';
-import { environment } from 'src/environments/environment';
-import { UtilService } from 'src/app/services/util/util.service';
-import { TipoProcesoService } from './tipo-proceso.service';
-import { UtilData } from 'src/app/services/util/util.data';
-import { InputService } from 'src/app/component/input/input.service';
-import { FormService } from 'src/app/component/form/form.service';
-import { MantenimientoService } from 'src/app/services/mantenimiento/mantenimiento.service';
+import { Component, OnInit } from "@angular/core";
+import { IColumnsTable } from "src/app/component/table/table.interface";
+import { TipoprocesoData } from "./tipo-proceso.data";
+import { ITipoproceso, ITipoprocesoTable } from "./tipo-proceso.interface";
+import Swal from "sweetalert2";
+import { HttpErrorResponse } from "@angular/common/http";
+import { IInputsComponent } from "src/app/component/input/input.interface";
+import { reportCodeEnum } from "src/app/services/util/util.interface";
+import { TableService } from "src/app/component/table/table.service";
+import { ValidationsService } from "src/app/services/validations/validations.service";
+import { environment } from "src/environments/environment";
+import { UtilService } from "src/app/services/util/util.service";
+import { TipoProcesoService } from "./tipo-proceso.service";
+import { UtilData } from "src/app/services/util/util.data";
+import { InputService } from "src/app/component/input/input.service";
+import { FormService } from "src/app/component/form/form.service";
+import { MantenimientoService } from "src/app/services/mantenimiento/mantenimiento.service";
 
 @Component({
-  templateUrl: './tipo-proceso.component.html',
+  templateUrl: "./tipo-proceso.component.html",
 })
 export class TipoProcesoComponent implements OnInit {
-
   public columnsTable: IColumnsTable = TipoprocesoData.columns;
   public dataTable: any[] = [];
-  public tableInputsEditRow: IInputsComponent = TipoprocesoData.tableInputsEditRow;
+  public tableInputsEditRow: IInputsComponent =
+    TipoprocesoData.tableInputsEditRow;
   public colsToFilterByText: string[] = TipoprocesoData.colsToFilterByText;
   public IdRowToClone: string = null;
-  public defaultEmptyRowTable: ITipoprocesoTable = TipoprocesoData.defaultEmptyRowTable;
+  public defaultEmptyRowTable: ITipoprocesoTable =
+    TipoprocesoData.defaultEmptyRowTable;
   public dataTipoSolicitudes: any[] = [];
-  public codigoReporte: reportCodeEnum = reportCodeEnum.MANTENIMIENTO_TIPO_PROCESO;
+  public codigoReporte: reportCodeEnum =
+    reportCodeEnum.MANTENIMIENTO_TIPO_PROCESO;
   constructor(
     private tipoprocesosService: TipoProcesoService,
     private tableService: TableService,
@@ -36,29 +38,31 @@ export class TipoProcesoComponent implements OnInit {
     private utilService: UtilService,
     private formService: FormService,
     private inputService: InputService,
-    private mantenimientoService: MantenimientoService,
+    private mantenimientoService: MantenimientoService
   ) {}
 
   ngOnInit(): void {
-    this.getDataToTable();
     this.getDataToCombo();
+    // this.getDataToTable();
   }
 
   private getDataToCombo() {
     return this.mantenimientoService.getTipoSolicitud().subscribe({
-      next: (response) => {
-        const comboTipoSolicitud = this.inputService.formatDataToOptionsValueInLabel(
-          response,
-          "tipoSolicitud",
-          "id"
-        );
-        this.dataTipoSolicitudes = response;
+      next: (response: any) => {
+        const comboTipoSolicitud =
+          this.inputService.formatDataToOptionsValueInLabel(
+            response.tipoSolicitudType,
+            "tipoSolicitud",
+            "id"
+          );
+        this.dataTipoSolicitudes = response.tipoSolicitudType;
         this.tableInputsEditRow = this.formService.changeValuePropFormById(
           "tipoSolicitudId",
           this.tableInputsEditRow,
           "options",
           comboTipoSolicitud
         );
+        this.getDataToTable();
       },
       error: (error: HttpErrorResponse) => {
         if (error.status !== 404) {
@@ -71,22 +75,25 @@ export class TipoProcesoComponent implements OnInit {
   private getDataToTable() {
     return this.tipoprocesosService.index().subscribe({
       next: (response) => {
-        this.dataTable = response.map((procesoResponse=>({
+        this.dataTable = response.map((procesoResponse) => ({
           ...procesoResponse,
           estado: procesoResponse.estado === "A",
-          tipoSolicitudFormatted:this.formatTipoSolicitudEstaciones(procesoResponse)
-        })));
+          tipoSolicitudFormatted:
+            this.formatTipoSolicitudEstaciones(procesoResponse),
+        }));
       },
       error: (error: HttpErrorResponse) =>
         this.utilService.modalResponse(error.error, "error"),
     });
   }
-  private formatTipoSolicitudEstaciones(procesoResponse:ITipoproceso):string{
-    const tipoSolcicitud=this.dataTipoSolicitudes.find(tipoSolcicitud=>tipoSolcicitud.id==procesoResponse.tipoSolicitudId);
-    if(tipoSolcicitud){
+  private formatTipoSolicitudEstaciones(procesoResponse: ITipoproceso): string {
+    const tipoSolcicitud = this.dataTipoSolicitudes.find(
+      (tipoSolcicitud) => tipoSolcicitud.id == procesoResponse.tipoSolicitudId
+    );
+    if (tipoSolcicitud) {
       return tipoSolcicitud.tipoSolicitud;
     }
-    return ' ';
+    return " ";
   }
   /**
    * Funcion para eliminar un registro
@@ -169,5 +176,4 @@ export class TipoProcesoComponent implements OnInit {
       this.onSaveRowTable(rowData, finishedClonningRow);
     }
   }
-
 }
