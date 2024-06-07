a67cf102d32f4e493e70cdd3465a446cc6f33826
LordCrainer <carlosgarcia.cagm@gmail.com>
2024-06-07 03:39:53 -0500

    vista(contratacion-familiar): [h15] Agregar datos de pruebas

--- src/app/solicitudes/registrar-familiares/registrar-familiares.data.ts
+++ src/app/solicitudes/registrar-familiares/registrar-familiares.data.ts
@@ -1,5 +1,52 @@
 import { IColumnsTable } from "src/app/component/table/table.interface";
 
+export const dataTableDatosFamiliares = [
+  {
+    name: "Nombre 1",
+    dateIn: "01/01/2021",
+    position: "Cargo 1",
+    unit: "Unidad 1",
+    departament: "Departamento 1",
+    location: "Localidad 1",
+    relationship: "Parentesco 1",
+  },
+  {
+    name: "Nombre 2",
+    dateIn: "02/02/2022",
+    position: "Cargo 2",
+    unit: "Unidad 2",
+    departament: "Departamento 2",
+    location: "Localidad 2",
+    relationship: "Parentesco 2",
+  },
+  {
+    name: "Nombre 3",
+    dateIn: "03/03/2023",
+    position: "Cargo 3",
+    unit: "Unidad 3",
+    departament: "Departamento 3",
+    location: "Localidad 3",
+    relationship: "Parentesco 3",
+  },
+  {
+    name: "Nombre 4",
+    dateIn: "04/04/2024",
+    position: "Cargo 4",
+    unit: "Unidad 4",
+    departament: "Departamento 4",
+    location: "Localidad 4",
+    relationship: "Parentesco 4",
+  },
+  {
+    name: "Nombre 5",
+    dateIn: "05/05/2025",
+    position: "Cargo 5",
+    unit: "Unidad 5",
+    departament: "Departamento 5",
+    location: "Localidad 5",
+    relationship: "Parentesco 5",
+  },
+];
 
 export const columnsDatosFamiliares: IConsultaTareasData = {
   columns: [
