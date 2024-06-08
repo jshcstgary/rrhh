a5d90a6c56477e937f60f2545fd38b18ca528136
LordCrainer <carlosgarcia.cagm@gmail.com>
2024-06-08 03:22:25 -0500

    vista(contratacion-familiar): [h15] Permite buscar los empleados por ev y jaff. Se agregó las rutas

--- src/app/solicitudes/registrar-familiares/buscar-empleados/buscar-empleados.component.ts
+++ src/app/solicitudes/registrar-familiares/buscar-empleados/buscar-empleados.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { catchError, map } from "rxjs";
+import { IEmpleados } from "src/app/services/mantenimiento/empleado.interface";
+import { MantenimientoService } from "src/app/services/mantenimiento/mantenimiento.service";
 
 interface BuscarEmpleadosFields {
   name: string;
@@ -12,6 +15,7 @@ interface BuscarEmpleadosFields {
   parentesco: string;
 }
 
+type Empleados = IEmpleados["evType"];
 @Component({
   selector: "app-buscar-empleados",
   templateUrl: "./buscar-empleados.component.html",
@@ -23,30 +27,52 @@ export class BuscarEmpleadosComponent {
   activeModal = inject(NgbActiveModal);
 
   searchInp: string;
-  fields: BuscarEmpleadosFields = {
-    name: "",
-    fechaIngreso: "",
-    cargo: "",
-    unidad: "",
+  fields = <Empleados[0]>{
+    nombreCompleto: "",
+    fechaIngresogrupo: null,
+    descrPuesto: "",
+    unidadNegocio: "",
     departamento: "",
     localidad: "",
-    parentesco: "",
   };
+
+  constructor(private mantenimientoService: MantenimientoService) {}
 
   ngOnInit(): void {}
 
-  onSeleccionar(){
+  onSeleccionar() {
     this.activeModal.close(this.fields);
   }
 
-  onEnter(): void {
-    // this.myService.miMetodo(this.searchInp).subscribe(
-    //   (response) => {
-    //     // Maneja la respuesta del servicio aquí
-    //   },
-    //   (error) => {
-    //     // Maneja el error aquí
-    //   }
-    // );
+  onEnter(search: string): void {
+    this.mantenimientoService
+      .getDataEmpleadosEvolution("ev")
+      .pipe(
+        map(this.buscarValor.bind(this, search, "jaffType")),
+        catchError((error) => {
+          return this.mantenimientoService
+            .getDataEmpleadosEvolution("jaff")
+            .pipe(map(this.buscarValor.bind(this, search, "jaffType")));
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          this.fields = data as Empleados[0];
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
   }
+
+  buscarValor = (search, type: "jaffType" | "evType", data: IEmpleados) => {
+    const result = data?.[type].find((item) => {
+      const regex = new RegExp(search, "i");
+      return item.nombreCompleto.match(regex);
+    });
+    if (!result) {
+      throw new Error("No se encontró el valor esperado");
+    }
+    return result;
+  };
 }
