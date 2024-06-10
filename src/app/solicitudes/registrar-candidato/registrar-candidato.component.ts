26f6ea9266a7d028066b20a3f0648ce97a6019e8
Miguel Castillo <miguel.castillo@iguanadigital.com.ec>
2024-06-10 17:59:21 -0500

    Selección de candidatos - contratación de familiares

--- src/app/solicitudes/registrar-candidato/registrar-candidato.component.ts
+++ src/app/solicitudes/registrar-candidato/registrar-candidato.component.ts
@@ -30,6 +30,7 @@ import {
 import { ConsultaTareasService } from "src/app/tareas/consulta-tareas/consulta-tareas.service";
 import { SolicitudesService } from "../registrar-solicitud/solicitudes.service";
 import { RegistrarCandidatoService } from "./registrar-candidato.service";
+import { TipoSolicitudService } from "src/app/mantenedores/tipo_solicitud/tipo-solicitud.service";
 
 @Component({
   selector: 'app-registrar-candidato',
@@ -90,6 +91,7 @@ export class RegistrarCandidatoComponent extends CompleteTaskComponent {
   isCheckedEntrevista: boolean = false;
 
   nombreCandidato: string = "";
+  codigoSolicitudProceso: string = "";
 
   toggleDivVisibility(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
@@ -390,7 +392,8 @@ export class RegistrarCandidatoComponent extends CompleteTaskComponent {
     private solicitudes: SolicitudesService,
     private utilService: UtilService,
     private consultaTareasService: ConsultaTareasService,
-    private seleccionCandidatoService: RegistrarCandidatoService
+    private seleccionCandidatoService: RegistrarCandidatoService,
+    private tipoSolicitudServicio: TipoSolicitudService
   ) {
     super(route, router, camundaRestService);
 
@@ -1175,27 +1178,27 @@ export class RegistrarCandidatoComponent extends CompleteTaskComponent {
       fuenteExterna: this.isChecked ? this.selectedOption : null,
       tipoProceso: this.model.tipoProceso,
       candidato: this.nombreCandidato,
-      actualizacionDelPerfil: this.fechas.actualizacionPerfil === "" ? null : this.fechas.actualizacionPerfil,
-      busquedaDeCandidatos: this.fechas.busquedaCandidatos === "" ? null : this.fechas.busquedaCandidatos,
-      entrevista: this.fechas.entrevista === "" ? null : this.fechas.entrevista,
-      pruebas: this.fechas.pruebas === "" ? null : this.fechas.pruebas,
-      referencias: this.fechas.referencias === "" ? null : this.fechas.referencias,
-      elaboracionDeInforme: this.fechas.elaboracionInforme === "" ? null : this.fechas.elaboracionInforme,
-      entregaAlJefeSol: this.fechas.entregaJefe === "" ? null : this.fechas.entregaJefe,
-      entrevistaPorJefatura: this.fechas.entrevistaJefatura === "" ? null : this.fechas.entrevistaJefatura,
-      tomaDeDesiciones: this.fechas.tomaDecisiones === "" ? null : this.fechas.tomaDecisiones,
-      candidatoSeleccionado: this.fechas.candidatoSeleccionado === "" ? null : this.fechas.candidatoSeleccionado,
-      procesoDeContratacion: this.fechas.procesoContratacion === "" ? null : this.fechas.procesoContratacion,
-      finProcesoContratacion: this.fechas.finProcesoContratacion === "" ? null : this.fechas.finProcesoContratacion,
+      actualizacionDelPerfil: this.fechas.actualizacionPerfil === "" ? null : new Date(this.fechas.actualizacionPerfil).toISOString(),
+      busquedaDeCandidatos: this.fechas.busquedaCandidatos === "" ? null : new Date(this.fechas.busquedaCandidatos).toISOString(),
+      entrevista: this.fechas.entrevista === "" ? null : new Date(this.fechas.entrevista).toISOString(),
+      pruebas: this.fechas.pruebas === "" ? null : new Date(this.fechas.pruebas).toISOString(),
+      referencias: this.fechas.referencias === "" ? null : new Date(this.fechas.referencias).toISOString(),
+      elaboracionDeInforme: this.fechas.elaboracionInforme === "" ? null : new Date(this.fechas.elaboracionInforme).toISOString(),
+      entregaAlJefeSol: this.fechas.entregaJefe === "" ? null : new Date(this.fechas.entregaJefe).toISOString(),
+      entrevistaPorJefatura: this.fechas.entrevistaJefatura === "" ? null : new Date(this.fechas.entrevistaJefatura).toISOString(),
+      tomaDeDesiciones: this.fechas.tomaDecisiones === "" ? null : new Date(this.fechas.tomaDecisiones).toISOString(),
+      candidatoSeleccionado: this.fechas.candidatoSeleccionado === "" ? null : new Date(this.fechas.candidatoSeleccionado).toISOString(),
+      procesoDeContratacion: this.fechas.procesoContratacion === "" ? null : new Date(this.fechas.procesoContratacion).toISOString(),
+      finProcesoContratacion: this.fechas.finProcesoContratacion === "" ? null : new Date(this.fechas.finProcesoContratacion).toISOString(),
       fechaInicioReingreso: this.model.tipoProceso !== 'reingresoPersonal' ? null : this.getCurrentDate(),
       fechaFinReingreso: null,
       fechaInicioContratacionFamiliares: this.model.tipoProceso !== 'contratacionFamiliares' ? null : this.getCurrentDate(),
       fechaFinContratacionFamiliares: null,
-      fechaIngresoCandidato : null
+      fechaIngresoCandidato: null
     };
 
     console.log(request);
-    return;
+    // return;
 
     this.seleccionCandidatoService.saveCandidato(request).subscribe({
       next: () => {
@@ -1236,14 +1239,69 @@ export class RegistrarCandidatoComponent extends CompleteTaskComponent {
           this.solicitud.idUnidadNegocio = this.model.unidadNegocio;
           this.solicitud.estadoSolicitud = "4";
           console.log("this.solicitud: ", this.solicitud);
-          this.solicitudes
-            .actualizarSolicitud(this.solicitud)
-            .subscribe((responseSolicitud) => {
-              console.log("responseSolicitud: ", responseSolicitud);
+          this.solicitudes.actualizarSolicitud(this.solicitud).subscribe((responseSolicitud) => {
+            console.log("responseSolicitud: ", responseSolicitud);
 
+            if (this.model.tipoProceso === "contratacionFamiliares" || this.model.tipoProceso === "reingresoPersonal") {
+              if (this.model.tipoProceso === "contratacionFamiliares") {
+                this.codigoSolicitudProceso = "CF";
+              } else if (this.model.tipoProceso === "reingresoPersonal") {
+                this.codigoSolicitudProceso = "RG";
+              }
 
+              this.tipoSolicitudServicio.index().subscribe(
+                {
+                  next: res => {
+                    const idTipoSolicitud = res.tipoSolicitudType.find(r => r.codigoTipoSolicitud === this.codigoSolicitudProceso).id;
 
-            });
+                    const solicitud = {
+                      fechaActualizacion: new Date(),
+                      fechaCreacion: new Date(),
+                      usuarioCreacion: "JOSHUA",
+                      usuarioActualizacion: "JOSHUA",
+                      estado: "3",
+                      idSolicitud: "string",
+                      idInstancia: this.solicitud.idInstancia,
+                      idEmpresa: this.solicitud.idEmpresa,
+                      empresa: this.solicitud.empresa,
+                      idUnidadNegocio: this.solicitud.idUnidadNegocio,
+                      unidadNegocio: this.solicitud.unidadNegocio,
+                      estadoSolicitud: "3",
+                      idTipoSolicitud,
+                      tipoSolicitud: "string",
+                      idTipoMotivo: 0,
+                      tipoMotivo: "string",
+                      idTipoAccion: 0,
+                      tipoAccion: "string"
+                    };
+
+                    this.solicitudes.guardarSolicitud(solicitud).subscribe({
+                      next: res => {
+                        console.log(res);
+
+                        this.solicitudes.getDetalleSolicitudById(this.solicitud.idSolicitud).subscribe({
+                          next: (detalleSolicitud) => {
+                            let detalle = detalleSolicitud.detalleSolicitudType[0];
+
+                            detalle = {
+                              ...detalle,
+                              idSolicitud: res.idSolicitud
+                            }
+
+                            this.solicitudes.guardarDetalleSolicitud(detalle).subscribe({
+                              next: (res) => {
+                                console.log(res);
+                              }
+                            });
+                          }
+                        });
+                      }
+                    });
+                  }
+                }
+              );
+            }
+          });
 
           this.utilService.closeLoadingSpinner();
           //fin actualizo la solicitud a enviada
