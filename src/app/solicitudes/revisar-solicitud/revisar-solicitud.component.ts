9a5420d0999840d0868c90294936d9e37bf41bb3
Cristhian Salcedo <cristhian.salcedo@iguanadigital.com.ec>
2024-06-18 04:16:53 +0000

    Update revisar-solicitud.component.ts

--- src/app/solicitudes/revisar-solicitud/revisar-solicitud.component.ts
+++ src/app/solicitudes/revisar-solicitud/revisar-solicitud.component.ts
@@ -1220,9 +1220,6 @@ export class RevisarSolicitudComponent extends CompleteTaskComponent {
 
     //variables.codigo = { value: this.model.codigo };
     //variables.idEmpresa = { value: this.model.idEmpresa };
-    if (this.solicitud.tipoSolicitud.toUpperCase().includes("REQUISICION")||this.solicitud.tipoSolicitud.toUpperCase().includes("REQUISICIÓN")||this.solicitud.tipoSolicitud.toUpperCase().includes("ACCION")||this.solicitud.tipoSolicitud.toUpperCase().includes("ACCIÓN")) {
-
-      //buttonValue
 
       if (this.taskType_Activity == environment.taskType_Revisar) { //APROBADORES DINAMICOS
 
@@ -1298,11 +1295,6 @@ export class RevisarSolicitudComponent extends CompleteTaskComponent {
 
         */
       }
-
-
-
-    }
-
 
     return { variables };
   }
