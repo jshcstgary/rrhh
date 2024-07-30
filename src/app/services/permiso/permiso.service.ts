e3c4b75d66ebbbd8f01a7029f018fd7d19142e68
Cristhian Salcedo <csalcedo@righttek.com>
2024-07-29 22:12:30 -0500

    Se valida doble perfil Iniciador y Admin

--- src/app/services/permiso/permiso.service.ts
+++ src/app/services/permiso/permiso.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { PageCodes } from 'src/app/enums/codes.enum';
 import { LocalStorageKeys } from 'src/app/enums/local-storage-keys.enum';
 import { Control, Permiso } from 'src/app/types/permiso.type';
 
@@ -10,11 +11,19 @@ export class PermisoService {
 
   public getPagePermission(pageCode: string): Control[] {
     const permissions: undefined | Permiso[] = JSON.parse(localStorage.getItem(LocalStorageKeys.Permisos));
-
     const permission: undefined | Permiso = permissions.find(permission => permission.codigo === pageCode);
+    const permissionsarray: undefined | Permiso[] = permissions.filter(permission => permission.codigo === pageCode);
 
     if (permission === undefined) {
       return [];
+    }
+    
+    if (permissionsarray.length > 1 && pageCode === PageCodes.ConsultaSolicitudes) {
+      permissionsarray.forEach((x) => {
+          if (x.controles[8].visualizar) {
+            permission.controles = x.controles;
+          }
+        });
     }
 
     return permission.controles;
