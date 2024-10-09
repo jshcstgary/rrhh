53f09d11f3230d70e9bc72c08fe4abd920191eea
Miguel Castillo <miguel.castillo@iguanadigital.com.ec>
2024-10-09 17:22:38 -0500

    Cambios a locales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ const base = "http://10.35.3.162"; //base de servicios local
 const baseRBP = "http://192.168.44.180"; //base de servicios RBP http://10.35.3.162
 const baseG = "http://181.188.224.250:18043"; //base de servicios api gateway
 
-export const pruebasLocales: boolean = false; //Varialbe boleana para las pruebas locales y pruebas en el ambiente del cliente, true para locales, false para RBP
+export const pruebasLocales: boolean = true; //Varialbe boleana para las pruebas locales y pruebas en el ambiente del cliente, true para locales, false para RBP
 
 // export const portalWorkFlow: string = "http://10.35.3.162:4200/";
 
